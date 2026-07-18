3b5a82d2d15bebdc7eb0931c528d0a14e83b5fe2
Halle Koyanagi <halleb10@gmail.com>
2026-07-17 20:07:10 -0700

    added fir filter implementation and test

--- include/tests.h
+++ include/tests.h
@@ -6,4 +6,6 @@
 void test_parser(char *output, input_data_t *input_data, filter_t *filter);
 void test_iir_filter(char *outputfile, input_data_t *input_data,
                      filter_t *filter);
+void test_fir_filter(char *outputfile, input_data_t *input_data,
+                     filter_t *filter);
 #endif
