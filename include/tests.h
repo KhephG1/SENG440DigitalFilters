200cdb59663b7222e506445becf15daa9a35a4e0
Khephren Gould <154032288+KhephG1@users.noreply.github.com>
2026-07-21 13:03:33 -0700

    fixes

--- include/tests.h
+++ include/tests.h
@@ -4,8 +4,8 @@
 #include "input_data_loader.h"
 #include <stdint.h>
 void test_parser(char *output, input_data_t *input_data, filter_t *filter);
-void test_iir_filter(char *outputfile, float *input_data, float *filter_x,
-                     float *filter_y, int *coeffs_x, int *coeffs_y);
+void test_iir_filter_float(char *outputfile, float *input_data, float *filter_x,
+                           float *filter_y, int *coeffs_x, int *coeffs_y);
 
 void test_iir_filter_fixed(char *outputfile, input_data_t *input_data,
                            filter_t *filter);
