6b5abb992ae6b6e3f3dd64d97ae0c0ea54969c84
Khephren Gould <154032288+KhephG1@users.noreply.github.com>
2026-07-25 10:19:26 -0700

    Neon implementation of IIR filter + test case (uses 20,000 input data_normalized.csv)

--- src/iir_filter_core.c
+++ src/iir_filter_core.c
@@ -2,9 +2,70 @@
 #include "coefficient_loader.h"
 #include "input_data_loader.h"
 #include "overflow_handler.h"
+#include <arm_neon.h>
 #include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
+
+void iir_filter_neon(const input_data_t *__restrict input,
+                     int16_t *__restrict output, uint32_t input_length,
+                     filter_t *__restrict filter)
+{
+
+    // compute the shifts to be applied (denominator coeffcients scaled with
+    // different factor than numerator coefficients)
+    const int num_sf = filter->num_scale_factor_exp;
+    const int den_sf = filter->den_scale_factor_exp;
+    if (num_sf != 22 || den_sf != 14)
+    {
+        printf("error vector operations require shifting by amount known at "
+               "compile time. if different scale factors used change the code");
+        return;
+    }
+    const int16_t *__restrict in = input->input_data_buffer;
+    // initialize the filter
+    if (input_length > 0)
+    {
+        int32_t temp1 = ((int32_t)filter->x[0] * in[0] + (1 << 21)) >> 22;
+        output[0] = saturate(temp1);
+    }
+    if (input_length > 1)
+    {
+        int32_t temp1 = ((int32_t)filter->x[0] * in[1] + (1 << 21)) >> 22;
+        int32_t temp2 = ((int32_t)filter->x[1] * in[0] + (1 << 21)) >> 22;
+        int32_t temp3 = ((int32_t)filter->y[1] * output[0] + (1 << 13)) >> 14;
+        int32_t total = temp1 + temp2 - temp3;
+        output[1] = saturate(total);
+    }
+    int16x4_t vx = {filter->x[2], filter->x[1], filter->x[0], 0};
+    int16x4_t vy = {filter->y[2], filter->y[1], 0, 0};
+    int32x4_t vround_num = vdupq_n_s32(1 << 21);
+    int32x4_t vround_den = vdupq_n_s32(1 << 13);
+    for (int i = 2; i < input_length; i++)
+    {
+        int16x4_t v_in = vld1_s16(in + i - 2);
+        int32x4_t res = vmull_s16(v_in, vx);
+
+        res = vaddq_s32(res, vround_num);
+        int32x4_t shifted = vshrq_n_s32(res, 22);
+        int32x2_t lo = vget_low_s32(shifted);
+        int32x2_t hi = vget_high_s32(shifted);
+        lo = vpadd_s32(lo, hi);
+        lo = vpadd_s32(lo, lo);
+        int32_t num_acc = vget_lane_s32(lo, 0);
+        int16x4_t v_out = vld1_s16(output + i - 2);
+        res = vmull_s16(v_out, vy);
+        res = vaddq_s32(res, vround_den);
+        shifted = vshrq_n_s32(res, 14);
+        lo = vget_low_s32(shifted);
+        hi = vget_high_s32(shifted);
+        lo = vpadd_s32(lo, hi);
+        lo = vpadd_s32(lo, lo);
+        int32_t den_acc = vget_lane_s32(lo, 0);
+        output[i] = saturate(num_acc - den_acc);
+    }
+}
+
 void iir_filter_fixed_point_with_unrolling(const input_data_t *__restrict input,
                                            int16_t *__restrict output,
                                            uint32_t input_length,
@@ -17,7 +78,7 @@ void iir_filter_fixed_point_with_unrolling(const input_data_t *__restrict input,
     const int acc_sf = (num_sf > den_sf) ? num_sf : den_sf;
     const int num_shift = acc_sf - num_sf;
     const int den_shift = acc_sf - den_sf;
-    const int64_t round_add = (int64_t)1 << (acc_sf - 1);
+    const int32_t round_add = (int32_t)1 << (acc_sf - 1);
 
     // store coefficients as local variables
     const int16_t x0 = filter->x[0];
@@ -31,30 +92,30 @@ void iir_filter_fixed_point_with_unrolling(const input_data_t *__restrict input,
     // goal of above is to get the compiler to keep coefficienst and accumulator
     // in registers
     const int16_t *__restrict in = input->input_data_buffer;
-    const int len = (int)input_length;
     // initialize the filter
-    if (len > 0)
+    if (input_length > 0)
     {
-        int64_t num_acc = (int64_t)x0 * in[0];
-        int64_t total = num_acc << num_shift;
-        total = (total + round_add) >> acc_sf;
+        int64_t num_acc = (int64_t)((int32_t)x0 * in[0]);
+        int64_t total = (num_acc + round_add) >> num_sf;
         output[0] = saturate(total);
     }
-    if (len > 1)
+    if (input_length > 1)
     {
-        int64_t num_acc = (int64_t)x0 * in[1] + (int64_t)x1 * in[0];
-        int64_t den_acc = (int64_t)y1 * output[0];
+        int64_t num_acc =
+            (int64_t)((int32_t)x0 * in[1]) + (int64_t)((int32_t)x1 * in[0]);
+        int64_t den_acc = (int64_t)((int32_t)y1 * output[0]);
         int64_t total = (num_acc << num_shift) - (den_acc << den_shift);
         total = (total + round_add) >> acc_sf;
         output[1] = saturate(total);
     }
 
-    for (int i = 2; i < len; i++)
+    for (int i = 2; i < input_length; i++)
     {
-        int64_t num_acc = (int64_t)x0 * in[i] + (int64_t)x1 * in[i - 1] +
-                          (int64_t)x2 * in[i - 2];
-        int64_t den_acc =
-            (int64_t)y1 * output[i - 1] + (int64_t)y2 * output[i - 2];
+        int64_t num_acc = (int64_t)((int32_t)x0 * in[i]) +
+                          (int64_t)((int32_t)x1 * in[i - 1]) +
+                          (int64_t)((int32_t)x2 * in[i - 2]);
+        int64_t den_acc = (int64_t)((int32_t)y1 * output[i - 1]) +
+                          (int64_t)((int32_t)y2 * output[i - 2]);
         int64_t total = (num_acc << num_shift) - (den_acc << den_shift);
         total = (total + round_add) >> acc_sf;
         output[i] = saturate(total);
@@ -78,8 +139,8 @@ void iir_filter_fixed_point(const input_data_t *input, int16_t *output,
         {
             if (i >= j)
             {
-                num_acc += (int64_t)filter->x[j] *
-                           (int64_t)input->input_data_buffer[i - j];
+                num_acc += (int64_t)((int32_t)filter->x[j] *
+                                     input->input_data_buffer[i - j]);
             }
         }
 
@@ -89,7 +150,7 @@ void iir_filter_fixed_point(const input_data_t *input, int16_t *output,
         {
             if (i >= j)
             {
-                den_acc += (int64_t)filter->y[j] * (int64_t)output[i - j];
+                den_acc += (int64_t)((int32_t)filter->y[j] * output[i - j]);
             }
         }
         // for addition must ensure same scale factors
