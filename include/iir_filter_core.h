aa85039f23d5188e224bbf43b91937920db69296
Khephren Gould <154032288+KhephG1@users.noreply.github.com>
2026-07-25 10:22:32 -0700

    Revert direct commit to main. oops :(
    This reverts commit 6b5abb992ae6b6e3f3dd64d97ae0c0ea54969c84.

--- include/iir_filter_core.h
+++ include/iir_filter_core.h
@@ -6,16 +6,14 @@
 #include <stddef.h>
 #include <stdint.h>
 
-void iir_filter_neon(const input_data_t *__restrict input,
-                     int16_t *__restrict output, uint32_t input_length,
-                     filter_t *__restrict filter);
-void iir_filter_fixed_point_pipelining(
-
-    ) void iir_filter_fixed_point_with_unrolling(const input_data_t
-                                                     *__restrict input,
-                                                 int16_t *__restrict output,
-                                                 uint32_t input_length,
-                                                 filter_t *__restrict filter);
+void iir_filter_fixed_point_with_unrolling(const input_data_t *__restrict input,
+                                           int16_t *__restrict output,
+                                           uint32_t input_length,
+                                           filter_t *__restrict filter);
 void iir_filter_fixed_point(const input_data_t *input, int16_t *output,
                             uint32_t input_length, filter_t *filter);
+void iir_filter_naive(const float *input, float *output, uint32_t input_length,
+                      float *filter_x, float *filter_y, int *coeffs_x,
+                      int *coeffs_y);
+
 #endif
