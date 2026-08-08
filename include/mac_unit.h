f10b2d03d4cebfd1144d9f509b685471a70e7b20
Khephren Gould <154032288+KhephG1@users.noreply.github.com>
2026-08-08 13:07:51 -0700

    revert

--- include/mac_unit.h
+++ include/mac_unit.h
@@ -3,16 +3,16 @@
 
 #include <stddef.h>
 #include <stdint.h>
-static inline int64_t mac(int64_t acc, int16_t a, int16_t b)
+
+static inline int32_t mac(int32_t acc, int16_t a, int16_t b)
 {
-    int32_t lo = (int32_t)(acc & 0xFFFFFFFF);
-    int32_t hi = (int32_t)(acc >> 32);
+    int32_t result;
 
-    __asm__("smlal %0, %1, %2, %3"
-            : "+r"(lo), "+r"(hi)
-            : "r"((int32_t)a), "r"((int32_t)b));
+    __asm__ volatile("mla %0, %1, %2, %3"
+                     : "=r"(result)
+                     : "r"((int32_t)a), "r"((int32_t)b), "r"(acc));
 
-    return ((int64_t)hi << 32) | (uint32_t)lo;
+    return result;
 }
 
 #endif
