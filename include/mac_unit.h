58ad646b893479a0397189255456ee1ee6753d36
Halle Koyanagi <halleb10@gmail.com>
2026-08-08 02:34:16 -0700

    made mac_unit inline

--- include/mac_unit.h
+++ include/mac_unit.h
@@ -4,6 +4,17 @@
 #include <stddef.h>
 #include <stdint.h>
 
-int32_t mac(int32_t acc, int16_t a, int16_t b);
+static inline int32_t mac(int32_t acc, int16_t a, int16_t b)
+{
+    int32_t result;
+
+    __asm__ volatile (
+        "mla %0, %1, %2, %3"
+        : "=r"(result)
+        : "r"((int32_t)a), "r"((int32_t)b), "r"(acc)
+    );
+
+    return result;
+}
 
 #endif
